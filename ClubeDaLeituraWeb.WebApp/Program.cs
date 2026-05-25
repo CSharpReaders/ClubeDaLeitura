9bc650745c04323d1e69886f6101a02a395c24b4
thig0S_ <thiagokovalski5@gmail.com>
2026-05-25 17:18:04 -0300

    Base do Projeto feita por Tiago Santini

---
+++ ClubeDaLeituraWeb.WebApp/Program.cs
@@ -0,0 +1,40 @@
+using ClubeDaLeituraWeb.WebApp.Compartilhado.Infra.Arquivos;
+using ClubeDaLeituraWeb.WebApp.ModuloCaixa.Dominio;
+using ClubeDaLeituraWeb.WebApp.ModuloCaixa.Infra;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Configuração de Serviços
+builder.Services.AddScoped(provider =>
+{
+    ContextoJson contextoJson = new ContextoJson();
+
+    contextoJson.Carregar();
+
+    return contextoJson;
+});
+
+builder.Services.AddScoped<IRepositorioCaixa, RepositorioCaixaEmArquivo>();
+
+builder.Services.AddControllersWithViews().AddRazorOptions(options =>
+{
+    // Resetar a configuração padrão do MVC
+    options.ViewLocationFormats.Clear();
+
+    // Views dos módulos: /ModuloCaixa/Apresentacao/Views/Listar.cshtml
+    options.ViewLocationFormats.Add("/Modulo{1}/Apresentacao/Views/{0}.cshtml");
+
+    // Views compartilhadas: /Compartilhado/Apresentacao/Views/_Layout.cshtml
+    options.ViewLocationFormats.Add("/Compartilhado/Apresentacao/Views/{0}.cshtml");
+});
+
+var app = builder.Build();
+
+// Configuração de Middlewares
+app.UseStaticFiles();
+
+app.UseRouting();
+app.MapDefaultControllerRoute();
+
+// Execução do App
+app.Run();
